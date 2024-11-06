b9da89b09a44d49253214d85730d7a5a1e77f224
jakejo <jakejo@kakao.com>
2024-11-06 09:48:41 +0900

    [REFACTOR] 미사용 UI 주석 처리

--- js/pages/main.js
+++ js/pages/main.js
@@ -6,6 +6,7 @@ function commonHandler(){
   commonSetting();
   commonSettingPopup();
   commonGenerateTemplate();
+  // commonSettingTextInclude();
 }
 // 프롬프트 설정 프롬프트 활성화
 function commonSetting(){
@@ -39,6 +40,113 @@ function commonSettingPopup(){
     $(popup).removeClass(isActive);
   })
 }
+// 프롬프트 설정 팝업 예시 텍스트 삽입
+function commonSettingTextInclude(){
+  const layer = $('.ly_pop_wrap')
+  const assetsBox = $('.ly_pop_wrap .ly_pop_assets_box');
+  const assetsType = $('.ly_pop_wrap .ly_pop_assets_box .ly_pop_assets_type');
+  const isActive = 'active';
+  let promptText = '';
+  let styleKeyword = '';
+  let subjectKeyword = '';
+  let framingKeyword = '';
+  let backgroundKeyword = '';
+  let lightingKeyword = '';
+  let cameraAngleKeyword = '';
+  let cameraPropertiesKeyword = ''
+  let filmTypesKeyword = '';
+  let lensesKeyword = ''
+  let filtersEffectsKeyword = '';
+  let photographersKeyword = ''
+  
+  styleHandler();
+  subjectHandler();
+  framingHandler();
+  backgroundHandler();
+  lightingHandler();
+  cameraAngleHandler();
+
+  // style
+  function styleHandler(){
+    // select
+    $(layer).eq(0).find(assetsBox).click(function(){
+      const keyword = $(this).find(assetsType).text();
+      styleKeyword = keyword;
+      $('#style').val(styleKeyword)
+      $(layer).removeClass(isActive);
+      console.log(styleKeyword)
+    })
+    // input 
+    $('#style').on('input', function(){
+      const inputText = $(this).val() + " photo of";
+      styleKeyword = inputText;
+      console.log(styleKeyword)
+    })
+  }
+  // subject
+  function subjectHandler(){
+    // input
+    $('#subject').on('input', function(){
+      const inputText = $(this).val();
+      subjectKeyword = inputText();
+    })
+  }
+  // framing
+  function framingHandler(){
+    // select
+    $(layer).eq(1).find(assetsBox).click(function(){
+      const keyword = $(this).find(assetsType).text();
+      framingKeyword = keyword;
+      $('#framing').val(framingKeyword);
+      $(layer).removeClass(isActive);
+    })
+    // input
+    $('#framing').on('input', function(){
+      const inputText = $(this).val();
+      framingKeyword = inputText;
+    })
+  }
+  // background
+  function backgroundHandler(){
+    // input
+    $('#background').on('input', function(){
+      const inputText = $(this).val();
+      backgroundKeyword = inputText;
+    })
+  }
+  // lighting
+  function lightingHandler(){
+    // select
+    $(layer).eq(2).find(assetsBox).click(function(){
+      const keyword = $(this).find(assetsType).text();
+      lightingKeyword = keyword;
+      $('#lighting').val(lightingKeyword);
+      $(layer).removeClass(isActive);
+    })
+    // input
+    $('#lighting').on('input', function(){
+      const inputText = $(this).val();
+      lightingKeyword = inputText;
+    })
+  }
+  // camera angle
+  function cameraAngleHandler(){
+    // select
+    $(layer).eq(3).find(assetsBox).click(function(){
+      const keyword = $(this).find(assetsType).text();
+      cameraAngleKeyword = keyword;
+      $('#camera_angle').val(cameraAngleKeyword);
+      $(layer).removeClass(isActive);
+    })
+    // input
+    $('#camera_angle').on('input', function(){
+      const inputText = $(this).val();
+      cameraAngleKeyword = inputText;
+    })
+  }
+  // c
+}
+// 이미지 생성 버튼
 function commonGenerateTemplate(){
   const generateBtn = $('.btn_generate_image');
   const generateArea = $('.section_generate');
